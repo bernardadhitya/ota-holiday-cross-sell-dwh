c3ac161319fcbc644c1ceeb34b23a7e346884787
Bernard Adhitya K <34474188+bernardadhitya@users.noreply.github.com>
2024-10-06 01:48:23 +0000

    Add fact tables mapping

---
+++ dwh/dwh_booking__fact_crosssell_transaction.sql
@@ -0,0 +1,28 @@
+WITH product_location AS (
+    SELECT
+        pc.product_id,
+        MIN(dl.location_id) AS location_id
+    FROM
+        ota_data_prod.raw_products__raw_product_catalog pc
+    JOIN ota_data_prod.dwh_products__dim_location dl
+        ON pc.product_name LIKE CONCAT('%', dl.city, '%')
+    GROUP BY pc.product_id
+)
+SELECT
+    rbb.bundle_booking_id AS crosssell_transaction_id,
+    rbb.customer_id,
+    ppc1.service_type_id AS primary_service_type_id,
+    ppc2.service_type_id AS secondary_service_type_id,
+    rbb.primary_product_id,
+    rbb.secondary_product_id,
+    rbb.booking_date AS transaction_date,
+    pl1.location_id,
+    dbo.bundle_offer_id,
+    rbb.bundle_price AS transaction_amount,
+    rbb.discount_applied AS bundle_discount_amount
+FROM
+    ota_data_prod.raw_booking__raw_bundle_booking rbb
+JOIN ota_data_prod.raw_products__raw_product_catalog ppc1 ON rbb.primary_product_id = ppc1.product_id
+JOIN ota_data_prod.raw_products__raw_product_catalog ppc2 ON rbb.secondary_product_id = ppc2.product_id
+LEFT JOIN product_location pl1 ON ppc1.product_id = pl1.product_id
+LEFT JOIN ota_data_prod.dwh_products__dim_bundle_offer dbo ON rbb.bundle_booking_id = dbo.bundle_offer_id
